fdf3e573c25a45563fe6ca1f8d9d3730a36e29b3
murugan <murugan001ab@gmail.com>
2025-03-27 23:56:18 +0530

    change api.jsx 4

--- src/App.jsx
+++ src/App.jsx
@@ -120,7 +120,21 @@ const handlyReset=()=>{
     <button className='start' onClick={()=>{
       setStart(false)
       handlyReset()
-    }}  >Start</button>
+    }}  >Start</button>import axios from "axios";
+
+    const API_BASE_URL = "https://8c83-2409-40f4-1016-1319-ad64-b874-a409-9b93.ngrok-free.app";
+    // const API_BASE_URL = "http://127.0.0.1:8000"
+    // // Django API URL
+    
+    export const saveUserScore = async (name, score) => {
+      return axios.post(`${API_BASE_URL}/users/`, { name, score });
+    };
+    
+    export const fetchQuestions = async () => {
+      const response = await axios.get(`${API_BASE_URL}/questions/`);
+      return response.data;
+    };
+    
     </div>
     </>):(<>
       {show ? (
