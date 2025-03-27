4f1521d15e3f81fc1767d41840aca211f20c24df
murugan <murugan001ab@gmail.com>
2025-03-27 15:32:49 +0530

    change api.jsx 1

--- src/api.jsx
+++ src/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://de52-2409-40f4-10f4-7292-4780-38e8-e29a-b73e.ngrok-free.app/"; // Django API URL
+const API_BASE_URL = "https://de52-2409-40f4-10f4-7292-4780-38e8-e29a-b73e.ngrok-free.app"; // Django API URL
 
 export const saveUserScore = async (name, score) => {
   return axios.post(`${API_BASE_URL}/users/`, { name, score });
